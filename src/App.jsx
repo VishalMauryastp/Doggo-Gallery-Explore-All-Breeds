a7aa6dc5be1ed53083c8deb9e4f33918e5a01281
Vishal Maurya <vishalmaurya12nov@gmil.com>
2023-11-22 16:41:12 +0530

    fixed bg issue

--- src/App.jsx
+++ src/App.jsx
@@ -6,22 +6,24 @@ const App = () => {
   const [breed, setBreed] = useState([]);
 
   return (
-    <div className={`bg-[url("/dogimg.jpg")]  bg-cover  min-h-screen`}>
-      <header>
-        <h1 className="md:text-3xl lg:text-[40px] font-bold py-2 px-8 text-center mt-12">
-          Doggo Gallery: Explore All Breeds
-        </h1>
-      </header>
-      <main>
-        <div>
-          <Home breed={breed} setBreed={setBreed} />
-          <Result breed={breed} setBreed={setBreed} />
-        </div>
-      </main>
-      <footer class="text-center text-gray-500 text-sm py-4">
-        {/* &copy; 2023 Your Company Name. All rights reserved. */}
-      </footer>
-    </div>
+    <>
+      <div className={` bg-[url("/mobileimg.jpg")] md:bg-[url("/tabimg.jpg")] xl:bg-[url("/dogimg.jpg")]  bg-cover min-h-[calc(100vh-50px)]`}>
+        <header>
+          <h1 className="md:text-3xl lg:text-[40px] font-bold py-2 px-8 text-center mt-12">
+            Doggo Gallery: Explore All Breeds
+          </h1>
+        </header>
+        <main>
+          <div>
+            <Home breed={breed} setBreed={setBreed} />
+            <Result breed={breed} setBreed={setBreed} />
+          </div>
+        </main>
+        <footer class="text-center text-gray-500 text-sm py-4">
+          {/* &copy; 2023 Your Company Name. All rights reserved. */}
+        </footer>
+      </div>
+    </>
   );
 };
 
