0a071d8fc44d8ab202485fa753cd5e90800ad58e
Vishal Maurya <vishalmaurya12nov@gmil.com>
2023-11-22 16:44:31 +0530

    done

--- src/App.jsx
+++ src/App.jsx
@@ -7,22 +7,22 @@ const App = () => {
 
   return (
     <>
-      <div className={` bg-[url("/mobileimg.jpg")] md:bg-[url("/tabimg.jpg")] xl:bg-[url("/dogimg.jpg")]  bg-cover min-h-[calc(100vh-50px)]`}>
-        <header>
-          <h1 className="md:text-3xl lg:text-[40px] font-bold py-2 px-8 text-center mt-12">
-            Doggo Gallery: Explore All Breeds
-          </h1>
-        </header>
-        <main>
-          <div>
-            <Home breed={breed} setBreed={setBreed} />
-            <Result breed={breed} setBreed={setBreed} />
-          </div>
-        </main>
-        <footer class="text-center text-gray-500 text-sm py-4">
-          {/* &copy; 2023 Your Company Name. All rights reserved. */}
-        </footer>
-      </div>
+      <header>
+        <h1 className="md:text-3xl lg:text-[40px] font-bold py-2 px-8 text-center mt-12">
+          Doggo Gallery: Explore All Breeds
+        </h1>
+      </header>
+      <main
+        className={` bg-[url("/mobileimg.jpg")] md:bg-[url("/tabimg.jpg")] xl:bg-[url("/dogimg.jpg")]  bg-cover min-h-[calc(100vh-50px)]`}
+      >
+        <div>
+          <Home breed={breed} setBreed={setBreed} />
+          <Result breed={breed} setBreed={setBreed} />
+        </div>
+      </main>
+      <footer class="text-center text-gray-500 text-sm py-4">
+        &copy; 2023 Doggo Gallery. All rights reserved.
+      </footer>
     </>
   );
 };
